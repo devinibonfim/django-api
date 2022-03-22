bfd6de392bc2cc9b450500934b3e033b4310eae7
Vinicius Bonfim <devinibonfim@gmail.com>
2022-03-22 11:27:55 -0300

    modificacao aluno admin

--- escola/admin.py
+++ escola/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from escola.models import Aluno
 
-# Register your models here.
+class Alunos(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'rg')
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome',)
+
+admin.site.register(Aluno, Alunos)
